f57227f79e762f75051cc4d66cc9e1951a911ce0
Максим Федоров <m88.fedorov@gmail.com>
2024-07-10 09:14:45 +0300

    update

--- Homework-01.07.2024-Files/Program.cs
+++ Homework-01.07.2024-Files/Program.cs
@@ -5,31 +5,27 @@
         static void Main(string[] args)
         {
             
-            //string[] drives = Directory.GetLogicalDrives();
+            string[] drives = Directory.GetLogicalDrives();
 
-            var root = new DirectoryInfo(@"D:\World_of_Tanks_EU");
-
-            Dictionary<string, int> extensionCount = [];
-            Dictionary<string, double> extensionSize = [];
-
+            //var root = new DirectoryInfo(@"D:\World_of_Tanks_EU");
+            var root = new DirectoryInfo(@"C:\Program Files (x86)");
+            SortedDictionary<string, int> extensionCount = [];
+            SortedDictionary<string, double> extensionSize = []; 
             var dirs = root.GetFiles("*", SearchOption.AllDirectories);
 
             foreach (var dir in dirs)
             {
                 string extension = dir.Extension.ToLower();
                 double fileSize = dir.Length;
-                if (!string.IsNullOrEmpty(extension))
+                if (extensionCount.ContainsKey(extension))
                 {
-                    if (extensionCount.ContainsKey(extension))
-                    {
-                        extensionCount[extension]++;
-                        extensionSize[extension] += fileSize;
-                    }
-                    else
-                    {
-                        extensionCount.Add(extension, 1);
-                        extensionSize.Add(extension, fileSize);
-                    }
+                    extensionCount[extension]++;
+                    extensionSize[extension] += fileSize;
+                }
+                else
+                {
+                    extensionCount.Add(extension, 1);
+                    extensionSize.Add(extension, fileSize);
                 }
             }
 
@@ -37,6 +33,7 @@
             Console.WriteLine("|  № |   расширение   |   количество   |   объём в байтах   |   % от кол-ва   |   % от объёма    |");
             Console.WriteLine("+----+----------------+----------------+--------------------+-----------------+------------------+");
 
+            var sortedDict = extensionCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
             int totalCount = 0;
             foreach (var total in extensionCount.Values)
             {
@@ -48,12 +45,16 @@
             {
                 totalBytes += file.Length;
             }
-
             int count = 0;
             double totalPercentCount = 0;
             double totalPercentBytes = 0;
-            foreach (var current in extensionCount)
+            foreach (var current in sortedDict)
             {
+                if (count >= 50)
+                {
+                    break;
+                }
+
                 double percentBytes = (double)extensionSize[current.Key] / totalBytes * 100;
                 double percentCount = (double)current.Value / totalCount * 100;
                 Console.Write("| {0, -3}", count + 1);
@@ -62,7 +63,7 @@
                 Console.Write("| {0, -19}", extensionSize[current.Key]);
                 Console.Write("| {0, -16}", Math.Round(percentCount, 2));
                 Console.Write("| {0, -17}", Math.Round(percentBytes, 2));
-                Console.WriteLine("| {0, -10}", "");
+                Console.WriteLine("|");
                 count++;
                 totalPercentCount += percentCount;
                 totalPercentBytes += percentBytes;
@@ -71,10 +72,11 @@
             Console.Write("| {0, -20}", "     ВСЕГО:      ");
             Console.Write("| {0, -15}", totalCount);
             Console.Write("| {0, -19}", totalBytes);
-            Console.Write("| {0, -16}", totalPercentCount);
-            Console.Write("| {0, -17}", totalPercentBytes);
-            Console.WriteLine("| {0, -10}", "");
+            Console.Write("| {0, -16}", Math.Round(totalPercentCount, 2));
+            Console.Write("| {0, -17}", Math.Round(totalPercentBytes, 2));
+            Console.WriteLine("|");
             Console.WriteLine("+----+----------------+----------------+--------------------+-----------------+------------------+");
+            
         }
     }
 }
